5b75845387b9059f9616e6b9c6d44cde01b0c96d
Hanhest <47452360+Hanhest@users.noreply.github.com>
2019-02-12 14:49:49 +0100

    Det jag gjort

---
+++ RollTo12/RollTo12/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace RollTo12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.lblDiceRoll = new System.Windows.Forms.Label();
+			this.tbxDice = new System.Windows.Forms.TextBox();
+			this.btnRollDice = new System.Windows.Forms.Button();
+			this.tbxSum = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblGeometric = new System.Windows.Forms.Label();
+			this.btnAll = new System.Windows.Forms.Button();
+			this.lblAll = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lblDiceRoll
+			// 
+			this.lblDiceRoll.AutoSize = true;
+			this.lblDiceRoll.Location = new System.Drawing.Point(89, 30);
+			this.lblDiceRoll.Name = "lblDiceRoll";
+			this.lblDiceRoll.Size = new System.Drawing.Size(153, 34);
+			this.lblDiceRoll.TabIndex = 0;
+			this.lblDiceRoll.Text = "Write a number over 4 \r\nand below 24:";
+			this.lblDiceRoll.Click += new System.EventHandler(this.lblDiceRoll_Click);
+			// 
+			// tbxDice
+			// 
+			this.tbxDice.Location = new System.Drawing.Point(320, 27);
+			this.tbxDice.Multiline = true;
+			this.tbxDice.Name = "tbxDice";
+			this.tbxDice.ReadOnly = true;
+			this.tbxDice.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tbxDice.Size = new System.Drawing.Size(372, 332);
+			this.tbxDice.TabIndex = 3;
+			// 
+			// btnRollDice
+			// 
+			this.btnRollDice.Location = new System.Drawing.Point(372, 397);
+			this.btnRollDice.Name = "btnRollDice";
+			this.btnRollDice.Size = new System.Drawing.Size(241, 73);
+			this.btnRollDice.TabIndex = 4;
+			this.btnRollDice.TabStop = false;
+			this.btnRollDice.Text = "Roll Dice";
+			this.btnRollDice.UseVisualStyleBackColor = true;
+			this.btnRollDice.Click += new System.EventHandler(this.btnRollDice_Click);
+			// 
+			// tbxSum
+			// 
+			this.tbxSum.Location = new System.Drawing.Point(117, 67);
+			this.tbxSum.Name = "tbxSum";
+			this.tbxSum.Size = new System.Drawing.Size(88, 22);
+			this.tbxSum.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(346, 362);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(311, 17);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Rolls four dices until you get your target number";
+			// 
+			// lblGeometric
+			// 
+			this.lblGeometric.AutoSize = true;
+			this.lblGeometric.Location = new System.Drawing.Point(48, 103);
+			this.lblGeometric.Name = "lblGeometric";
+			this.lblGeometric.Size = new System.Drawing.Size(240, 17);
+			this.lblGeometric.TabIndex = 7;
+			this.lblGeometric.Text = "Sum up all dice rolls on the latest try:";
+			// 
+			// btnAll
+			// 
+			this.btnAll.Enabled = false;
+			this.btnAll.Location = new System.Drawing.Point(117, 140);
+			this.btnAll.Name = "btnAll";
+			this.btnAll.Size = new System.Drawing.Size(88, 45);
+			this.btnAll.TabIndex = 6;
+			this.btnAll.Text = "All";
+			this.btnAll.UseVisualStyleBackColor = true;
+			this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+			// 
+			// lblAll
+			// 
+			this.lblAll.AutoSize = true;
+			this.lblAll.Location = new System.Drawing.Point(135, 212);
+			this.lblAll.Name = "lblAll";
+			this.lblAll.Size = new System.Drawing.Size(0, 17);
+			this.lblAll.TabIndex = 9;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 494);
+			this.Controls.Add(this.lblAll);
+			this.Controls.Add(this.btnAll);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lblGeometric);
+			this.Controls.Add(this.tbxSum);
+			this.Controls.Add(this.btnRollDice);
+			this.Controls.Add(this.tbxDice);
+			this.Controls.Add(this.lblDiceRoll);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDiceRoll;
+        private System.Windows.Forms.TextBox tbxDice;
+        private System.Windows.Forms.Button btnRollDice;
+        private System.Windows.Forms.TextBox tbxSum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGeometric;
+        private System.Windows.Forms.Button btnAll;
+		private System.Windows.Forms.Label lblAll;
+	}
+}
+
---
+++ RollTo12/RollTo12/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RollTo12
+{
+    public partial class Form1 : Form
+    {
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        int num4 = 0;
+        Random rnd = new Random();
+        int[] all;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void btnRollDice_Click(object sender, EventArgs e)
+        {
+
+            tbxDice.Clear();
+            int sum = 0;
+            int insert = int.Parse(tbxSum.Text);
+			int tries = 1;
+
+            if (insert > 3 && insert < 25)
+            {
+				while (sum != insert)
+				{
+
+					all = new int[tries];
+					for (int i = 0 ; sum != insert ; i++)
+					{
+						int num1 = rnd.Next(1, 7);
+						tbxDice.AppendText("Dice Roll Num.1 = " + num1.ToString() + " \n");
+						int num2 = rnd.Next(1, 7);
+						tbxDice.AppendText("Dice Roll Num.2 = " + num2.ToString() + " \n");
+						int num3 = rnd.Next(1, 7);
+						tbxDice.AppendText("Dice Roll Num.3 = " + num3.ToString() + " \n");
+						int num4 = rnd.Next(1, 7);
+						tbxDice.AppendText("Dice Roll Num.4 = " + num4.ToString() + " \n");
+						sum = num1 + num2 + num3 + num4;
+						tbxDice.AppendText("Summan =" + sum + " \n" + " \n");
+						tries++;
+					}
+				}
+				tbxDice.AppendText("Försök = " + tries + " \n" + " \n");
+			}
+
+            else
+            {
+                tbxDice.AppendText("Thats Tha Wrong Numba \n");
+            }
+            btnRollDice.Enabled = false;
+            tbxSum.Enabled = false;
+            btnAll.Enabled = true;
+        }
+
+        private void btnFigure_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDiceRoll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnAll_Click(object sender, EventArgs e)
+        {
+
+			lblAll.Text = "" + all[0]; 
+            btnRollDice.Enabled = true;
+            tbxSum.Enabled = true;
+        }
+    }
+}
+    
+
+
